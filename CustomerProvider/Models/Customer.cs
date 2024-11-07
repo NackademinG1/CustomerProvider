38a912ae9413dd106549cb9ac42910d4edeaeddb
Cecilia Sporrong <ccf.sporrong@hotmail.com>
2024-11-07 14:18:44 +0100

    init

---
+++ CustomerProvider/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomerProvider.Models;
+
+public class Customer
+{
+    [Required(ErrorMessage = "Du måste fylla i ditt förnamn")]
+    public string FirstName { get; set; } = null!;
+
+    [Required(ErrorMessage = "Du måste fylla i ditt efternamn")]
+    public string LastName { get; set;} = null!;
+
+    [Required(ErrorMessage = "Du måste fylla i din email")]
+    public string Email { get; set; } = null!;
+}
